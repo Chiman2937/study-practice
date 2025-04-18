6bf6d6930b7431b23d9f959b5c05bca748916f35
Chiman2937 <cyga2933@gmail.com>
2025-04-18 15:46:44 +0900

    fix: float 예제1,2 수정, feat: BFC 예제-mdn 추가

--- css-float 예제1/index.js
+++ css-float 예제1/index.js
@@ -2,17 +2,23 @@ const div2 = document.querySelector('.div2');
 const btn_left = document.querySelector('#left');
 const btn_none = document.querySelector('#none');
 
+const txt_float = document.createElement('span');
+txt_float.textContent = 'float:none';
+div2.append(txt_float);
+
 function click_left(e) {
   btn_none.disabled = false;
   btn_left.disabled = true;
   div2.classList.add('float-left');
   div2.classList.remove('float-none');
+  txt_float.textContent = 'float:left';
 }
 function click_none(e) {
   btn_left.disabled = false;
   btn_none.disabled = true;
   div2.classList.add('float-none');
   div2.classList.remove('float-left');
+  txt_float.textContent = 'float:none';
 }
 
 btn_left.addEventListener('click', click_left);
--- css-float 예제2/index.js
+++ css-float 예제2/index.js
@@ -5,17 +5,23 @@ const btn_none = document.querySelector('#none');
 const btn_long = document.querySelector('#long-text');
 const btn_short = document.querySelector('#short-text');
 
+const txt_float = document.createElement('span');
+txt_float.textContent = 'float:none';
+div2.append(txt_float);
+
 function click_left(e) {
   btn_none.disabled = false;
   btn_left.disabled = true;
   div2.classList.add('float-left');
   div2.classList.remove('float-none');
+  txt_float.textContent = 'float:left';
 }
 function click_none(e) {
   btn_left.disabled = false;
   btn_none.disabled = true;
   div2.classList.add('float-none');
   div2.classList.remove('float-left');
+  txt_float.textContent = 'float:none';
 }
 
 function click_longtext(e) {
